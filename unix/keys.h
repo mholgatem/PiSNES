be11129e34d29af468020111cb6926abe85023f8
mholgatem <holgate.mark1@gmail.com>
2014-08-06 15:35:51 -0700

    2 Player
    2 player with xinmo support

---
+++ unix/keys.h
@@ -0,0 +1,53 @@
+// Default key and joystick mappings
+
+#define RPI_KEY_A	SDLK_d
+#define RPI_KEY_B	SDLK_c
+#define RPI_KEY_X	SDLK_s
+#define RPI_KEY_Y	SDLK_x
+#define RPI_KEY_L	SDLK_a
+#define RPI_KEY_R	SDLK_f
+#define RPI_KEY_START	SDLK_RETURN
+#define RPI_KEY_SELECT	SDLK_TAB
+#define RPI_KEY_LEFT	SDLK_LEFT
+#define RPI_KEY_RIGHT	SDLK_RIGHT
+#define RPI_KEY_UP	SDLK_UP
+#define RPI_KEY_DOWN	SDLK_DOWN
+#define RPI_KEY_QUIT	SDLK_ESCAPE
+#define RPI_KEY_ACCEL	SDLK_BACKSPACE
+
+#define RPI_KEY_A_2	SDLK_q
+#define RPI_KEY_B_2	SDLK_w
+#define RPI_KEY_X_2	SDLK_e
+#define RPI_KEY_Y_2	SDLK_r
+#define RPI_KEY_L_2	SDLK_t
+#define RPI_KEY_R_2	SDLK_t
+#define RPI_KEY_START_2	SDLK_y
+#define RPI_KEY_SELECT_2	SDLK_u
+#define RPI_KEY_LEFT_2	SDLK_i
+#define RPI_KEY_RIGHT_2	SDLK_o
+#define RPI_KEY_UP_2	SDLK_p
+#define RPI_KEY_DOWN_2	SDLK_j
+#define RPI_KEY_QUIT_2	SDLK_k
+#define RPI_KEY_ACCEL_2	SDLK_l
+
+#define RPI_JOY_A_1	0
+#define RPI_JOY_B_1	3
+#define RPI_JOY_X_1	1
+#define RPI_JOY_Y_1	2
+#define RPI_JOY_L_1	17
+#define RPI_JOY_R_1	16
+#define RPI_JOY_START_1	11
+#define RPI_JOY_SELECT_1	10
+#define RPI_JOY_QUIT	8
+#define RPI_JOY_ACCEL	9
+#define RPI_JOY_QLOAD	18
+#define RPI_JOY_QSAVE	19
+
+#define RPI_JOY_A_2	4
+#define RPI_JOY_B_2	5
+#define RPI_JOY_X_2	6
+#define RPI_JOY_Y_2	7
+#define RPI_JOY_L_2	14
+#define RPI_JOY_R_2	15
+#define RPI_JOY_START_2	12
+#define RPI_JOY_SELECT_2	22
